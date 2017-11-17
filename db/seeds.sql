1bb6d83f2650fea4adc56a90f9aa3ccb3eb37867
Daniel Lucio <Daniel@Daniels-MacBook-Air-2.local>
2017-11-16 19:07:21 -0600

    all the files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+### Seeds
+
+USE ratings_db;
+
+INSERT INTO people (name) VALUES ('Hit Me With Your Best Shallot Burger');
+INSERT INTO comments (name) VALUES ('I Came I Saw the Coleslaw Burger');
+
+
+SELECT * FROM people;
+SELECT * FROM comments; 
+
+ 
